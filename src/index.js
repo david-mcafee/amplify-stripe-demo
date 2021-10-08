2dd5b8969427806baec1f174852cfb6f8c785141
David McAfee <mcafd@amazon.com>
2021-10-08 15:03:14 -0700

    add lambda function; import Stripe for fraud detection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
+import "@stripe/stripe-js";
 
 Amplify.configure(awsExports);
 
@@ -12,7 +13,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
